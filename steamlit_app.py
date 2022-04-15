5d13746d8cc4f19cfff55b8bd45a14f14edbd379
Jonathan Hardwick <103676557+JRHevolv@users.noreply.github.com>
2022-04-15 10:25:58 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.title("My Mom\'s New Healthy Diner")
 
@@ -17,3 +20,4 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+streamlit.text(fruityvice_response)
