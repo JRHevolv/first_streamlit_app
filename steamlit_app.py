e11487648a320e9d5fcc9a0bcee5ee100e579c27
Jonathan Hardwick <103676557+JRHevolv@users.noreply.github.com>
2022-04-15 10:27:45 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.title("My Mom\'s New Healthy Diner")
 
@@ -21,3 +20,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 streamlit.text(fruityvice_response)
+
+streamlit.header('Fruityvice Fruit Advice!')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
